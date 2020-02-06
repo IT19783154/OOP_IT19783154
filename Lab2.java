44847804f755766b9730ca409b78087072cc9a31
IT19783154 <yashikaumali@gmail.com>
2020-02-06 20:24:42 +0530

    LAB2 QUS 1 ANSWERS

---
+++ Lab2.java
@@ -0,0 +1,15 @@
+class lab2{
+
+public static void main(String args[])
+{
+	int miles,yards;
+	double kilometers;
+	miles=26;
+	yards=385;
+	kilometers=miles*1.609;
+	System.out.println ("26miles="+kilometers+"km");
+	kilometers=yards/1760.0*1.609;
+	System.out.println ("385yards="+kilometers+"km");
+	
+}
+}
